8b019204ab91e02528a3a43657b305fd0db33eb5
vfwes <vfwes@VFWIII>
2022-07-20 22:57:53 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+    internal class Building
+    {
+        private string _designer = "Wes Westmoreland";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+
+        public double Volume => Width * Depth * (3 * Stories);
+
+        //public int GetStories() { return Stories; }
+        //public void SetStories(int num) { Stories = num; }
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string buyer)
+        {
+            _owner = buyer;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine($@"
+{_address}-------------------
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Own by {_owner}
+{Volume} cubic meters of space");
+        }
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace UrbanPlanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building myHouse = new Building("709 Nowhere Lane");
+            myHouse.Width = 45;
+            myHouse.Depth = 54;
+            myHouse.Stories = 3;
+
+            Building herHouse = new Building("2909 Cornwallis");
+            herHouse.Width = 29.1;
+            herHouse.Depth = 21.9;
+            herHouse.Stories = 2;
+
+            Building hisHouse = new Building("1106 Grayland");
+            hisHouse.Width = 15.9;
+            hisHouse.Depth = 10.7;
+            hisHouse.Stories = 1;
+
+            Building theirHouse = new Building("1 Park Glenn");
+            theirHouse.Width = 25.3;
+            theirHouse.Depth = 75.8;
+            theirHouse.Stories = 2;
+
+            myHouse.Construct();
+            herHouse.Construct();
+            hisHouse.Construct();
+            theirHouse.Construct();
+
+            myHouse.Purchase("Mikelon");
+            herHouse.Purchase("Jeff Wheyzos");
+            hisHouse.Purchase("Wesser Weasel");
+            theirHouse.Purchase("Logie Lox");
+
+            myHouse.printInfo();
+            Console.WriteLine();
+            herHouse.printInfo();
+            Console.WriteLine();
+            hisHouse.printInfo();
+            Console.WriteLine();
+            theirHouse.printInfo();
+            
+
+
+        }
+    }
+}
